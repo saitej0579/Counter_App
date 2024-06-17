959726e4ce1b8a076dcc4b8ab4cf0cefde165560
sai teja <yanagandulasaiteja0579@gmail.com>
2024-06-17 20:06:45 +0530

    final commit

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ function increment(){
     let value = parseInt(text.textContent);
     value++;
     text.innerText = value; 
+    setTimeout(function(){
+        clicking();
+    },600)
+    text2.innerText = "clicked";
 };
 
 function decrement(){
     let value = parseInt(text.textContent);
     value = value - 1;
     text.innerText = value; 
+    setTimeout(function(){
+        clicking();
+    },600)
+    text2.innerText = "clicked";
 };
 
 
+const text2 = document.querySelector('#click');
+function clicking(){
+    text2.innerText = "";
+}
 
